f8f82803428567898cee1482f1813c3b53178e52
phantuananh1206 <phantuananhltt@gmail.com>
2020-12-04 10:40:15 +0700

    Static Pages

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,6 @@ module RubyOe39FoodAndDrink
     config.load_defaults 6.0
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
   end
 end
